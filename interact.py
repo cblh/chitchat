d88caa485fbb8b669d5e82a8af1d58030975ba4e
burlinchen <chenbeiliuhuo@gmail.com>
2024-07-31 15:38:25 +0800

    chore: update .gitignore and clean up interact.py
    - Added `sample/samples.txt` to .gitignore.
    - Removed unnecessary import of `SummaryWriter` from `interact.py`.

--- interact.py
+++ interact.py
@@ -5,7 +5,6 @@ import json
 import random
 import numpy as np
 import argparse
-from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 from tqdm import tqdm
 from torch.nn import DataParallel
